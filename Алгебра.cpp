0e37f41e2c540ddae9bac042991c71a9058cb70e
Jakepps <nagalevsiy@mail.ru>
2021-12-13 22:07:40 +0300

    Добавьте файлы проекта.

---
+++ Алгебра.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int n;
+	float a[50][50];
+
+	cout << "Введите размер матрицы: " << endl;
+	cin >> n;
+	cout << "Введите элементы матрицы:" << endl;
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			cin >> a[i][j];
+
+	for (int i = 1; i < n; ++i)
+		for (int k = i; k < n; ++k)
+			for (int j = n - 1; j >= 0; --j)
+				if (a[k][i - 1] != 0 && a[i - 1][i - 1] != 0 && a[i - 1][j] != 0)
+					a[k][j] -=((a[k][i - 1] / a[i - 1][i - 1]) * a[i - 1][j]);
+
+	cout << "Верхне-треугольный вид матрицы:" << endl;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << a[i][j] << " ";
+		}
+		cout << endl;
+	}
+	system("pause");
+}
